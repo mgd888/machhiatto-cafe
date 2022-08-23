703c147cdcaad5de1fc77a71788643937d86fe65
Maria De leon <mariadeleon@marias-mbp.gateway.sasktel.net>
2022-08-22 22:16:30 -0600

    inital

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../css/App.css';
+import Navbar from './Navbar'
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
